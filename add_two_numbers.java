f61738576db4d8bc4991dfed45448588e9a38d2b
FhdRasheed <145442940+FhdRasheed@users.noreply.github.com>
2023-09-19 16:15:43 +0530

    Add files via upload
    Addition of two numbers

---
+++ add_two_numbers.java
@@ -0,0 +1,10 @@
+package java_s3_061;
+
+public class add_two_numbers {
+	public static void main(String [] args) {
+		int num1=10,num2=20;
+		int sum=num1+num2;
+		System.out.print("sum="+sum);
+	}
+
+}
