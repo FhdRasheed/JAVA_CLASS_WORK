03ccfd73371fe42968fea9838c0b9027bd563a03
FhdRasheed <145442940+FhdRasheed@users.noreply.github.com>
2023-09-19 16:20:30 +0530

    Add files via upload
    Division of two numbers

---
+++ division.java
@@ -0,0 +1,9 @@
+package java_s3_061;
+
+public class division {
+	public static void main(String [] args) {
+		float num1=10,num2=20;
+		float div=num1/num2;
+		System.out.print("result="+div);
+	}
+}
