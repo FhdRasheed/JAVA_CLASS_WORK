ca6add4581a6b2c5d3e5e496d493f483c6df8b9d
FhdRasheed <145442940+FhdRasheed@users.noreply.github.com>
2023-09-19 15:06:00 +0530

    Add files via upload
    My First Java Program

---
+++ Hellojava.java
@@ -0,0 +1,17 @@
+package java_s3_061;
+/***************************************************************************************
+ * File                    :  Hellojava.java
+ * Discription             :  My First Java Program that Prints "Hello java"
+ * Author                  :  Fahad Rasheed
+ * Version                 :  1.0
+ * Date                    :  19/09/2023
+ * *********************************************************************************************
+ */
+
+public class Hellojava {
+	public static void main(String [] args ) {
+	System.out.print("Hello java");
+	System.out.println("Hello felix");
+
+}
+}
