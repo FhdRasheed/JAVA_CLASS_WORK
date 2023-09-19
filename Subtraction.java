8b7ce82997bfc0a1ad31e75d61094584d074e97a
FhdRasheed <145442940+FhdRasheed@users.noreply.github.com>
2023-09-19 16:18:33 +0530

    Add files via upload
    Difference of two numbers

---
+++ Subtraction.java
@@ -0,0 +1,10 @@
+package java_s3_061;
+
+public class Subtraction {
+	public static void main(String [] args) {
+		int number1=5,number2=4;
+		int difference=number1-number2;
+		System.out.print("difference="+difference);
+	}
+
+}
