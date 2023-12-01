eff866066628f36136a5c5aed67910fd9eb4a635
FhdRasheed <145442940+FhdRasheed@users.noreply.github.com>
2023-12-01 11:29:57 +0530

    Add files via upload

---
+++ ThreadMultiplicationTable.java
@@ -0,0 +1,93 @@
+/***************************************************************************************
+ * File                    :  ThreadMultiplicationTable.java
+ * Description             :  The java code uses three threads to concurrently print the 
+ *                            multiplication table of three user-input numbers. 
+ * Author                  :  Fahad Rasheed
+ * Version                 :  1.0
+ * Date                    :  1/12/2023
+ * *********************************************************************************************
+ */
+package new_java_lab;
+
+import java.util.Scanner;
+
+class printTable{
+	public synchronized void printMultiplicationTable(int Number) {
+		for(int i=1;i<=10;i++) {
+			System.out.println(i+"x"+Number+"="+i*Number);
+		}
+		
+	}
+}
+
+class Thread1 extends Thread{
+	printTable print ;
+	int number1;
+	Thread1(printTable m,int number1){
+		this.print=m;
+		this.number1=number1;
+		
+		
+	}
+	public void run() {
+		print.printMultiplicationTable(number1);
+		System.out.println();
+	}
+	
+	
+}
+class Thread2 extends Thread{
+	printTable print ;
+	int number2;
+	Thread2(printTable n,int number2){
+		this.print=n;
+		this.number2=number2;
+	
+}
+	public void run() {
+		print.printMultiplicationTable(number2);
+		System.out.println();
+	}
+	
+}
+class Thread3 extends Thread{
+	printTable print ;
+	int number3;
+	Thread3(printTable n,int number3){
+		this.print=n;
+		this.number3=number3;
+	
+}
+	public void run() {
+		print.printMultiplicationTable(number3);
+		System.out.println();
+	}
+	
+}	
+
+public class ThreadMultiplicationTable {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the table you want to run by Thread1");
+		int firstNumber=sc.nextInt();
+		System.out.println("Enter the table you want to run by Thread2");
+		int secondNumber=sc.nextInt();
+		System.out.println("Enter the table you want to run by Thread3");
+		int thirdNumber=sc.nextInt();
+
+
+		printTable table=new printTable();
+		Thread1 thread1=new Thread1(table,firstNumber);
+		Thread2 thread2=new Thread2(table,secondNumber);
+		Thread3 thread3=new Thread3(table,thirdNumber);
+		/*thread1.setPriority(3);
+		thread2.setPriority(2);
+		thread3.setPriority(1);*/
+		thread1.start();
+		thread2.start();
+		thread3.start();
+		
+	}
+	
+
+}
